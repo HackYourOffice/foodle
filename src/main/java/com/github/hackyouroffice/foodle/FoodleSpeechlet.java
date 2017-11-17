7296970375669c384c6af0f6489c1e1914ee26ff
Fabian Buch <fabian@fabian-buch.com>
2017-11-17 12:07:07 +0100

    first food proposal

--- src/main/java/com/github/hackyouroffice/foodle/FoodleSpeechlet.java
+++ src/main/java/com/github/hackyouroffice/foodle/FoodleSpeechlet.java
@@ -1,7 +1,12 @@
 package com.github.hackyouroffice.foodle;
 
 import com.amazon.speech.json.SpeechletRequestEnvelope;
+import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.*;
+import com.amazon.speech.ui.OutputSpeech;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+import com.amazon.speech.ui.SimpleCard;
 
 public class FoodleSpeechlet implements SpeechletV2 {
 
@@ -17,11 +22,80 @@ public class FoodleSpeechlet implements SpeechletV2 {
 
   @Override
   public SpeechletResponse onIntent(SpeechletRequestEnvelope<IntentRequest> speechletRequestEnvelope) {
-    return null;
+    IntentRequest request = speechletRequestEnvelope.getRequest();
+//    log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
+//        requestEnvelope.getSession().getSessionId());
+
+    Intent intent = request.getIntent();
+    String intentName = (intent != null) ? intent.getName() : null;
+
+    if ("LunchProposal".equals(intentName)) {
+
+      return getAskResponse("Essensvorschlag", "Essen gibt's da drüben!");
+    } else {
+      return getAskResponse("HelloWorld", "This is unsupported. Please try something else.");
+    }
   }
 
   @Override
   public void onSessionEnded(SpeechletRequestEnvelope<SessionEndedRequest> speechletRequestEnvelope) {
 
+  }
+
+  /**
+   * Helper method for retrieving an Ask response with a simple card and reprompt included.
+   *
+   * @param cardTitle  Title of the card that you want displayed.
+   * @param speechText speech text that will be spoken to the user.
+   * @return the resulting card and speech text.
+   */
+  private SpeechletResponse getAskResponse(String cardTitle, String speechText) {
+    SimpleCard card = getSimpleCard(cardTitle, speechText);
+    PlainTextOutputSpeech speech = getPlainTextOutputSpeech(speechText);
+    Reprompt reprompt = getReprompt(speech);
+
+    return SpeechletResponse.newAskResponse(speech, reprompt, card);
+  }
+
+  /**
+   * Helper method that creates a card object.
+   *
+   * @param title   title of the card
+   * @param content body of the card
+   * @return SimpleCard the display card to be sent along with the voice response.
+   */
+  private SimpleCard getSimpleCard(String title, String content) {
+    SimpleCard card = new SimpleCard();
+    card.setTitle(title);
+    card.setContent(content);
+
+    return card;
+  }
+
+  /**
+   * Helper method for retrieving an OutputSpeech object when given a string of TTS.
+   *
+   * @param speechText the text that should be spoken out to the user.
+   * @return an instance of SpeechOutput.
+   */
+  private PlainTextOutputSpeech getPlainTextOutputSpeech(String speechText) {
+    PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+    speech.setText(speechText);
+
+    return speech;
+  }
+
+  /**
+   * Helper method that returns a reprompt object. This is used in Ask responses where you want
+   * the user to be able to respond to your speech.
+   *
+   * @param outputSpeech The OutputSpeech object that will be said once and repeated if necessary.
+   * @return Reprompt instance.
+   */
+  private Reprompt getReprompt(OutputSpeech outputSpeech) {
+    Reprompt reprompt = new Reprompt();
+    reprompt.setOutputSpeech(outputSpeech);
+
+    return reprompt;
   }
 }
