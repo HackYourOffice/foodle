50c806304592faa767d8753fd1350b03589ccf0f
Fessele, Marcus <Marcus.Fessele@dm.de>
2017-11-17 20:34:14 +0100

    fix test for missing session and compile errors for proposal

--- src/test/java/com/github/hackyouroffice/foodle/FoodleSpeechletTest.java
+++ src/test/java/com/github/hackyouroffice/foodle/FoodleSpeechletTest.java
@@ -2,7 +2,10 @@ package com.github.hackyouroffice.foodle;
 
 import com.amazon.speech.json.SpeechletRequestEnvelope;
 import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.Application;
 import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -20,10 +23,12 @@ public class FoodleSpeechletTest {
   @Test
   public void onIntent() throws Exception {
 
-    when(lunchProposer.getProposal()).thenReturn(new Proposal("foo", "bar"));
+    when(lunchProposer.getProposal()).thenReturn(
+            new Proposal("foo",
+                    new Location("bar", "address", 1, true, false)
+            ));
 
     final FoodleSpeechlet foodleSpeechlet = new FoodleSpeechlet(lunchProposer);
-
     final Intent intent = Intent.builder().withName("LunchProposal").build();
 
     foodleSpeechlet.onIntent(wrapIntend(intent));
@@ -33,8 +38,14 @@ public class FoodleSpeechletTest {
 
   private SpeechletRequestEnvelope<IntentRequest> wrapIntend(Intent intent) {
     final IntentRequest intentRequest = IntentRequest.builder().withRequestId("1").withIntent(intent).build();
+    Session session =
+            Session.builder()
+                    .withSessionId("sId")
+                    .withApplication(new Application("applicationId"))
+                    .withUser(new User("UserId"))
+                    .build();
     return SpeechletRequestEnvelope.<IntentRequest>builder()
-          .withRequest(intentRequest).build();
+          .withRequest(intentRequest).withSession(session).build();
   }
 
 }
